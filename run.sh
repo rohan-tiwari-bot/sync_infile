6ebfafc113c098f35b579bf80f31ae9cd103f36b
Rohan <rohantwari1@gmail.com>
2019-02-28 17:20:39 +0000

    sync commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cut -d ',' -f 1,2,3,5 /root/symmetrical-fiesta-scrapers/youtube-one-to-one/youtube_channels.csv > /root/sync_infile/yt.csv
+cut -d ',' -f 3,5 /root/symmetrical-fiesta-scrapers/youtube-one-to-one/youtube_channels.csv > /root/sync_infile/yt.csv
 cut -d ',' -f 1,3 /root/sharechat-instagram-scraper/users.csv > /root/sync_infile/ig.csv
 cut -d ',' -f 1,2,3 /root/symmetrical-fiesta-scrapers/facebook-scraper/profiles.csv > /root/sync_infile/fb.csv
 
